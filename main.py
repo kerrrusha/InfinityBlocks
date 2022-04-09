f29e6d54b13876e8eca57fb71e5c74f05e3f870d
Kirill <73041091+kerrrusha@users.noreply.github.com>
2022-04-10 00:41:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from sys import platform as PLATFORM
+from os import environ
 from infinityBlocks import InfinityBlocks
+__version__ = "0.1"
+
+def platform():
+    if 'ANDROID_ARGUMENT' in environ:
+        return "android"
+    elif PLATFORM in ('linux', 'linux2','linux3'):
+        return "linux"
+    elif PLATFORM in ('win32', 'cygwin'):
+        return 'win'
+
+if platform() == "android":
+    path = "/data/data/org.test.pgame/files/app/"
+elif platform() == "linux":
+    path = "./"
 
 game = InfinityBlocks()
 game.run()
